e58bf128a20f1c7d1a611a1b8d62f3ea1aafc7dc
DalbertSanchez <sdalbert34@gmail.com>
2024-07-22 15:31:30 -0400

    Principio Abierto/Cerrado

---
+++ PrincipiosSolid/OnlineCourse.cs
@@ -0,0 +1,10 @@
+﻿namespace PrincipiosSolid
+{
+    public class OnlineCouse : Course
+    {
+        public override void Subscribe(Student student)
+        {
+            Console.WriteLine($"Estudiante {student.Name} te has suscrito al curso online\n\n");
+        }
+    }
+}
--- PrincipiosSolid/Program.cs
+++ PrincipiosSolid/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PrincipiosSolid;
+
+var student = new Student { StudentId = 20230213, Name = "Dalbert Sanchez" };
+
+
+Course online = new OnlineCouse { CourseId = 4005, Tittle = "Curso online de fundamentos de programacion" };
+Course offline = new OfflineCourse { CourseId = 1505, Tittle = "Curso offline de Programacion I" };
+Course hybrid = new HybridCourse { CourseId = 2589, Tittle = "Curso hybrido de fundamentos de programacion / Programacion I" };
+
+Console.WriteLine(online.Tittle);
+online.Subscribe(student);
+
+
+Console.WriteLine(offline.Tittle);
+offline.Subscribe(student);
+
+
+Console.WriteLine(hybrid.Tittle);
+hybrid.Subscribe(student);
