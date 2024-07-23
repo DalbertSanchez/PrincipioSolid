3c74e62226aa2c4af778e813d7e16638a6cc2898
DalbertSanchez <sdalbert34@gmail.com>
2024-07-22 23:05:42 -0400

    Principio de Sustitucion de Liskov

--- PrincipiosSolid/Program.cs
+++ PrincipiosSolid/Program.cs
@@ -3,17 +3,17 @@
 var student = new Student { StudentId = 20230213, Name = "Dalbert Sanchez" };
 
 
-Course online = new OnlineCouse { CourseId = 4005, Tittle = "Curso online de fundamentos de programacion" };
-Course onsiteCourse = new OnsiteCourse { CourseId = 1505, Tittle = "Curso presencial de Programacion I" };
-Course hybrid = new HybridCourse { CourseId = 2589, Tittle = "Curso hybrido de fundamentos de programacion / Programacion I" };
+Course course = new OnlineCouse { CourseId = 4005, Tittle = "Curso online de fundamentos de programacion" };
+Console.WriteLine(course.Tittle);
+course.Subscribe(student);
 
-Console.WriteLine(online.Tittle);
-online.Subscribe(student);
+course = new OnsiteCourse { CourseId = 1505, Tittle = "Curso presencial de Programacion I" };
+Console.WriteLine(course.Tittle);
+course.Subscribe(student);
+
+course = new HybridCourse { CourseId = 2589, Tittle = "Curso hybrido de fundamentos de programacion / Programacion I" };
+Console.WriteLine(course.Tittle);
+course.Subscribe(student);
 
 
-Console.WriteLine(onsiteCourse.Tittle);
-onsiteCourse.Subscribe(student);
 
-
-Console.WriteLine(hybrid.Tittle);
-hybrid.Subscribe(student);
